269773a763dda8fad34c3e9d5c7632617027f891
Diego Pulido <diegopulido384@gmail.com>
2022-04-09 01:43:51 -0500

    finished the handler of the components (except the filters) and completed the funcionalities of the task cards (delete and check)

--- src/components/BodyApp.js
+++ src/components/BodyApp.js
@@ -5,32 +5,62 @@ class BodyApp extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			todoQueue: [],
+			allTasks: [],
 		};
-	}
-	render() {
-		return (
-			<div className='app-body__container'>
-				<TextAreaToDo onKeyUp={e => this.getTextTodo(e)}></TextAreaToDo>
-				<ToDoList list={this.state.todoQueue}></ToDoList>
-			</div>
-		);
 	}
 
 	getTextTodo(textArea) {
 		if (textArea.key === 'Enter' && textArea.target.value) {
+			const task = {
+				value: textArea.target.value,
+				isCompleted: false,
+			};
 			this.setState({
-				todoQueue: this.state.todoQueue.concat(textArea.target.value),
+				allTasks: this.state.allTasks.concat(task),
 			});
 			textArea.target.value = '';
 		}
+	}
+
+	completedTaskState(element) {
+		const taskUpdated = [...this.state.allTasks];
+		taskUpdated[element].isCompleted = !taskUpdated[element].isCompleted;
+		this.setState({
+			allTasks: taskUpdated,
+		});
+	}
+
+	deleteTask(elem) {
+		const taskUpdated = [...this.state.allTasks];
+		taskUpdated.splice(elem, 1);
+		this.setState({
+			allTasks: taskUpdated,
+		});
+	}
+
+	handlerCleanList() {
+		this.setState({ allTasks: [] });
+	}
+
+	render() {
+		return (
+			<div className='app-body__container'>
+				<TextAreaToDo onKeyUp={e => this.getTextTodo(e)}></TextAreaToDo>
+				<ToDoList
+					list={this.state.allTasks}
+					handlerCleanList={() => this.handlerCleanList()}
+					completedTsk={e => this.completedTaskState(e)}
+					deleteTask={e => this.deleteTask(e)}
+				></ToDoList>
+			</div>
+		);
 	}
 }
 
 function TextAreaToDo(props) {
 	return (
 		<div className='new-task-input__container'>
-			<span className='new-task-input__btn'></span>
+			<span className='new-task-input__btn element-completeTask__btn'></span>
 			<input
 				className='new-task__input'
 				onKeyUp={e => props.onKeyUp(e)}
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,15 +1,68 @@
-function ToDoList(props) {
-	let globalId = 0;
-	const arrOfElements = props.list.map(e => (
-		<li key={globalId++} className='list-element__container'>
-			<TodoElement element={e}></TodoElement>
-		</li>
-	));
-	return <ul className='todo-list__container'>{arrOfElements}</ul>;
+import React from 'react';
+
+class ToDoList extends React.Component {
+	constructor(props) {
+		super(props);
+		this.globalId = 0;
+	}
+
+	render() {
+		const arrOfElements = this.props.list.map((e, i) => (
+			<li key={this.globalId++} className='list-element__container'>
+				<TodoElement
+					element={e}
+					completedTsk={() => this.props.completedTsk(i)}
+					deleteTask={() => this.props.deleteTask(i)}
+				></TodoElement>
+			</li>
+		));
+		return (
+			<ul className='todo-list__container'>
+				{arrOfElements}
+				{this.props.list.length > 0 && (
+					<AllListHandler
+						numItems={this.props.list.length}
+						handlerCleanList={this.props.handlerCleanList}
+					></AllListHandler>
+				)}
+			</ul>
+		);
+	}
 }
 
 function TodoElement(props) {
-	return <p>{props.element}</p>;
+	const completed = props.element.isCompleted ? 'isCompleted' : '';
+	return (
+		<div className='task-elements__container'>
+			<div
+				className={`element-completeTask__btn ${completed}`}
+				onClick={() => props.completedTsk()}
+			></div>
+			<p className={completed && completed + 'btn'}>
+				{props.element.value}
+			</p>
+			<button
+				className='element-hidden-delete__btn'
+				onClick={() => props.deleteTask()}
+			></button>
+		</div>
+	);
+}
+
+function AllListHandler(props) {
+	return (
+		<li className='list-element__container list-handler__container '>
+			<span>{`${props.numItems} item${
+				props.numItems > 1 ? 's' : ''
+			} left`}</span>
+			<button
+				className='listHandler-clear__btn'
+				onClick={() => props.handlerCleanList()}
+			>
+				Clear Completed
+			</button>
+		</li>
+	);
 }
 
 export default ToDoList;
